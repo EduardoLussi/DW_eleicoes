9a48cf63bd325fd096432576c12d610fe43a94f5
Eduardo Lussi <eduardolussi@gmail.com>
2022-11-07 09:18:29 -0300

    4h/2M

--- import-data.py
+++ import-data.py
@@ -2,6 +2,10 @@ import mysql.connector
 import pandas as pd
 import numpy as np
 from sys import stdout
+import time
+
+cpfs = []
+candidato_ids = []
 
 # ----- Conexão com MySQL
 try:
@@ -19,7 +23,7 @@ print('Connected to database!')
 cursor = conn.cursor(buffered=True)
 
 # ----- Leitura dados votos
-data = pd.read_csv('./data/2022-SC.csv', sep=';', encoding='cp1252', nrows=1000, # Número de linhas a serem lidas para testes
+data = pd.read_csv('./data/2022-SC.csv', sep=';', encoding='cp1252', # Número de linhas a serem lidas para testes
                    usecols=['ANO_ELEICAO', 'NR_TURNO', 'SG_UF', 'NM_MUNICIPIO',
                             'NR_ZONA', 'NR_SECAO', 'DS_CARGO_PERGUNTA', 'NR_VOTAVEL', 
                             'SG_PARTIDO', 'NM_VOTAVEL', 'QT_VOTOS', 'QT_COMPARECIMENTO',
@@ -57,6 +61,9 @@ eleicao_id = cursor.fetchone()[0]
 # Inicializa variáveis para otimização
 local_id, uf, municipio, zona, secao = "", "", "", "", ""
 
+votos = []
+start_time = time.time()
+
 # ----- Iterar sobre votos
 for index, row in data.iterrows():
     stdout.write(f"\r{index+1}/{data_size}")
@@ -89,7 +96,7 @@ for index, row in data.iterrows():
     partido, numero, cargo = row['SG_PARTIDO'], row['NR_VOTAVEL'], row['DS_CARGO_PERGUNTA']
 
     if tipo_votavel in ('Nulo', 'Branco'):
-        cpf = cargo # Somente um candidato Branco e um Nulo por cargo
+        cpf = cargo + tipo_votavel[0] # Somente um candidato Branco e um Nulo por cargo
         partido = nome # Partido de Branco é Branco, de Nulo é Nulo
     else:
         # Obtém informações do candidato
@@ -103,37 +110,56 @@ for index, row in data.iterrows():
 
         cpf = candidato.iloc[0]['NR_CPF_CANDIDATO']
 
-    # --- Insere candidato
-    cursor.execute(f"INSERT INTO candidato (cpf, nome) \
-                     SELECT * FROM (SELECT '{cpf}' AS cpf, '{nome}' AS nome) AS tmp \
-                     WHERE NOT EXISTS (SELECT cpf, nome FROM candidato \
-                                       WHERE cpf='{cpf}' AND nome='{nome}')")
-    
-    # --- Insere candidatura
-    # Obtém id do candidato
-    cursor.execute(f"SELECT id FROM candidato WHERE cpf='{cpf}' AND nome='{nome}'")
-    candidato_id = cursor.fetchone()[0]
+    # Insere candidato e candidatura somente se já não estiver inserido
+    if cpf not in cpfs:
+        # --- Insere candidato
+        cursor.execute(f"INSERT INTO candidato (cpf, nome) \
+                        SELECT * FROM (SELECT '{cpf}' AS cpf, '{nome}' AS nome) AS tmp \
+                        WHERE NOT EXISTS (SELECT cpf, nome FROM candidato \
+                                        WHERE cpf='{cpf}' AND nome='{nome}')")
+        
+        # --- Insere candidatura
+        # Obtém id do candidato
+        cursor.execute(f"SELECT id FROM candidato WHERE cpf='{cpf}' AND nome='{nome}'")
+        candidato_id = cursor.fetchone()[0]
 
-    cursor.execute(f"INSERT INTO candidatura \
-                     SELECT * FROM \
-                        (SELECT {candidato_id} AS candidato_id, {eleicao_id} AS eleicao_id, \
-                                '{cargo}' AS cargo, '{partido}' AS partido, \
-                                '{numero}' AS numero) AS tmp \
-                     WHERE NOT EXISTS (SELECT * FROM candidatura \
-                                       WHERE candidato_id={candidato_id} AND \
-                                             eleicao_id={eleicao_id})")
+        cursor.execute(f"INSERT INTO candidatura \
+                        SELECT * FROM \
+                            (SELECT {candidato_id} AS candidato_id, {eleicao_id} AS eleicao_id, \
+                                    '{cargo}' AS cargo, '{partido}' AS partido, \
+                                    '{numero}' AS numero) AS tmp \
+                        WHERE NOT EXISTS (SELECT * FROM candidatura \
+                                        WHERE candidato_id={candidato_id} AND \
+                                                eleicao_id={eleicao_id})")
     
-    # --- Inserir tabela fato sem atributos derivados    
-    # Insere fato somente com quantidade
-    cursor.execute(f"INSERT INTO voto \
-                     SELECT * FROM \
-                        (SELECT {turno_id} AS turno_id, {eleicao_id} AS eleicao_id, \
-                                {candidato_id} AS candidato_id, {local_id} AS local_id, \
-                                {row['QT_VOTOS']} AS quantidade, NULL AS porcentagem_cargo, \
-                                NULL AS porcentagem_valido_cargo) AS tmp \
-                     WHERE NOT EXISTS (SELECT * FROM voto \
-                                       WHERE turno_id={turno_id} AND eleicao_id={eleicao_id} AND \
-                                             candidato_id={candidato_id} AND local_id={local_id})")
+        cpfs.append(cpf)
+        candidato_ids.append(candidato_id)
+    else:
+        candidato_id = candidato_ids[cpfs.index(cpf)]
+
+    votos.append((turno_id, eleicao_id, candidato_id, local_id, row['QT_VOTOS'],
+                  None, None, turno_id, eleicao_id, candidato_id, local_id
+        ))
+
+    if index and index % 1000 == 0:
+        # --- Inserir tabela fato sem atributos derivados          
+        sql = f"INSERT INTO voto \
+                    SELECT * FROM \
+                        (SELECT %s AS turno_id, %s AS eleicao_id, \
+                                %s AS candidato_id, %s AS local_id, \
+                                %s AS quantidade, %s AS porcentagem_cargo, \
+                                %s AS porcentagem_valido_cargo) AS tmp \
+                    WHERE NOT EXISTS (SELECT * FROM voto \
+                                    WHERE turno_id=%s AND eleicao_id=%s AND \
+                                            candidato_id=%s AND local_id=%s);"
+                
+        # Insere fato somente com quantidade
+        cursor.executemany(sql, votos)
+        votos.clear()
+    
+    if index % 50000 == 0:
+        conn.commit()
+print(f"\nTempo de execução: {time.time() - start_time:.2f}s")
 
 conn.commit()
 conn.close()
